d6e2c3e49d24db97031b2d63f0ecda800036c0bf
Newt Fourie <newt.fourie@entelect.co.za>
2022-07-14 21:28:17 +0200

    Actual data example

---
+++ actualData.js
@@ -0,0 +1,118 @@
+const milestones = [
+    {
+        "milestoneId": 1,
+        "projectId": 1,
+        "name": "Milestone 1",
+        "description": "Milestone 1",
+        "plannedDate": "2022-05-01T00:00:00Z",
+        "baselinePlannedDate": "2022-05-01T00:00:00Z"
+    },
+    {
+        "milestoneId": 35,
+        "projectId": 1,
+        "name": "May 2",
+        "plannedDate": "2022-05-02T00:00:00Z",
+        "baselinePlannedDate": "2022-05-02T00:00:00Z"
+    },
+    {
+        "milestoneId": 36,
+        "projectId": 1,
+        "name": "May 3",
+        "plannedDate": "2022-05-03T00:00:00Z",
+        "baselinePlannedDate": "2022-05-03T00:00:00Z"
+    },
+    {
+        "milestoneId": 37,
+        "projectId": 1,
+        "name": "May 4",
+        "plannedDate": "2022-05-04T00:00:00Z",
+        "baselinePlannedDate": "2022-05-04T00:00:00Z"
+    },
+    {
+        "milestoneId": 30,
+        "projectId": 1,
+        "name": "June 1",
+        "plannedDate": "2022-06-01T00:00:00Z",
+        "baselinePlannedDate": "2022-06-01T00:00:00Z"
+    },
+    {
+        "milestoneId": 34,
+        "projectId": 1,
+        "name": "June 2",
+        "plannedDate": "2022-06-02T00:00:00Z",
+        "baselinePlannedDate": "2022-06-02T00:00:00Z"
+    }
+];
+
+const deliverables = [
+    {
+        "deliverableId": 1,
+        "name": "Deliverable 1",
+        "description": "Deliverable 1",
+        "startDate": "2022-05-03T00:00:00Z",
+        "endDate": "2022-05-26T00:00:00Z",
+        "projectId": 1
+    },
+    {
+        "deliverableId": 9,
+        "name": "Custome Management",
+        "description": "All month",
+        "startDate": "2022-06-01T00:00:00Z",
+        "endDate": "2022-06-30T00:00:00Z",
+        "projectId": 1
+    },
+    {
+        "deliverableId": 8,
+        "name": "Design June",
+        "description": "Designing",
+        "startDate": "2022-06-01T00:00:00Z",
+        "endDate": "2022-06-14T00:00:00Z",
+        "projectId": 1
+    },
+    {
+        "deliverableId": 4,
+        "name": "June Sprint 1",
+        "description": "First Sprint",
+        "startDate": "2022-06-01T00:00:00Z",
+        "endDate": "2022-06-07T00:00:00Z",
+        "projectId": 1
+    },
+    {
+        "deliverableId": 5,
+        "name": "June Sprint 2",
+        "description": "Second Sprint",
+        "startDate": "2022-06-08T00:00:00Z",
+        "endDate": "2022-06-14T00:00:00Z",
+        "projectId": 1
+    },
+    {
+        "deliverableId": 10,
+        "name": "Analysis for July",
+        "description": "Starting Analysis",
+        "startDate": "2022-06-10T00:00:00Z",
+        "endDate": "2022-06-20T00:00:00Z",
+        "projectId": 1
+    },
+    {
+        "deliverableId": 6,
+        "name": "June Sprint 3",
+        "startDate": "2022-06-15T00:00:00Z",
+        "endDate": "2022-06-21T00:00:00Z",
+        "projectId": 1
+    },
+    {
+        "deliverableId": 7,
+        "name": "June Finish",
+        "description": "Final push",
+        "startDate": "2022-06-21T00:00:00Z",
+        "endDate": "2022-06-30T00:00:00Z",
+        "projectId": 1
+    },
+    {
+        "deliverableId": 3,
+        "name": "Deliverable 2",
+        "startDate": "2022-07-01T00:00:00Z",
+        "endDate": "2022-07-14T00:00:00Z",
+        "projectId": 1
+    }
+];
